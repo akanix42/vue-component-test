961b9f57a108aa9cfd9ea1d71d03b662907883f1
Nathan Reid <nathan-code@nathantreid.com>
2017-06-29 22:21:01 -0500

    test dynamic import

--- client/imports/App.vue
+++ client/imports/App.vue
@@ -8,6 +8,7 @@
 <div class="app">
   <span>Main App</span>
   <eager></eager>
+  <dynamic></dynamic>
 </div>
 </template>
 
@@ -17,6 +18,7 @@ import Eager from '/client/Eager.vue';
 export default {
   components: {
     Eager,
+    Dynamic: async () => (await import('./Dynamic.vue')).default,
   },
 }
 </script>
---
+++ client/imports/Dynamic.vue
@@ -0,0 +1,17 @@
+<style module>
+.dynamic {
+  color: red;
+}
+</style>
+
+<template>
+<div>
+  Dynamicly loaded vue file
+</div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
